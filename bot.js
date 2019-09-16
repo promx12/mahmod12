2eb2d6f023a47d7f621c70a8fe4eb3efa16fa648
promx12 <55059276+promx12@users.noreply.github.com>
2019-09-16 23:05:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,4 +10,6 @@ channel.send(`@@@@@@@@@`);
 }, 30)
 })
 
-client.login(process.env.BOT_TOKEN);
+client.login(NjE0MjY4OTA0OTE4NzQ1MDkw.XX_2iw.s5yzo92WmBSruYSISjfzbfWXUCA);
+client.login(NjA5NzQxNDI4NzQ2MjIzNjE3.XX_43w.zTw2xPf_Q4GuGkrjbMfzeZxt9LU);
+
